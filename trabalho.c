ce7a9c58a8c19ccbc1bec332a012f16e1b5e5c72
CalelBecassi <55924822+CalelBecassi@users.noreply.github.com>
2019-11-07 12:14:52 -0400

    Update trabalho.c
    adicionando teodoro (ainda não finalizado)

--- trabalho.c
+++ trabalho.c
@@ -2,28 +2,63 @@
 #include<stdint.h>
 #include<stdlib.h>
 #include<time.h>
-#include<math.h>
+#include<string.h>
 
-int mdc(int x, int y){
+typedef __uint128_t gint;
 
-
-}
-
-void fabio(/*int n, int s, int v*/){
+/*void fabio(char tarefa){
 
 	unsigned long int r;
 
-	while(mdc(r, n) != 1){
 
-		srand((unsigned)time(NULL));
-		r = rand();
+	if(tarefa == 'I'){
+
 	}
 
+	else if(tarefa == 'X'){
 
+		scanf()
+
+		while(mdc(r, n) != 1){
+
+			srand((unsigned)time(NULL));
+			r = rand();
+		}
+	}
+
+	else if(tarefa == 'R'){
+
+	}
+
+	else if(tarefa == 'T'){
+
+	}
+
+}*/
+
+void teodoro(char tarefa){
+
+	unsigned long int p, q;
+	char num[50];
+	__uint128_t n;
+
+	if(tarefa == 'I'){
+
+		scanf("%lu", &p);
+		printf("%lu\n", p );
+		scanf("%lu", &q);
+
+		n = 340282366920938463463374607431768;
+
+		sprintf(num, "%lu", n);
+
+		printf("%s\n", num);
+	}
 }
 
-
 int main(int argc, char *argv[]){
+
+	teodoro('I');
 
 	return 0;
 }
