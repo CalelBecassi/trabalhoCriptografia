8c10dc5fa3bdbd0f1cdfcfd9c40940b4f655c24d
CalelBecassi <55924822+CalelBecassi@users.noreply.github.com>
2019-11-07 09:17:29 -0400

    Add files via upload

---
+++ trabalho.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+int mdc(int x, int y){
+
+
+}
+
+void fabio(/*int n, int s, int v*/){
+
+	unsigned long int r;
+
+	while(mdc(r, n) != 1){
+
+		srand((unsigned)time(NULL));
+		r = rand();
+	}
+
+
+}
+
+
+int main(int argc, char *argv[]){
+
+	return 0;
+}
