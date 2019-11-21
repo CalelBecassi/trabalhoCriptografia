716a2997a8bc66c62e10f36eaaa19844b5786cca
Calel Becassi Benis <201819040666@lab01-l150.dominio.facom.ufms.br>
2019-11-21 16:11:01 -0300

    atualizando main, colocando switch cases

--- trabalho.c
+++ trabalho.c
@@ -22,91 +22,106 @@ unsigned long int mdc(int x, int y){
     	return x;
 	}
 
-void fabio(char tarefa){
+void fabio(){
 
+	char tarefa;
 	unsigned long int r, x, n, s, v, y;
 	int b;
 
-	if(tarefa == 'I'){
+	while(tarefa != 'T'){
 
-		scanf("%lu %lu %lu", &n, &s, &v);
+		scanf("%c", &tarefa);
 
-		if(((((s * s) % n) * v) % n) == 1){
-			printf("C\n");
-			verificado = true;
-		}
-		else
-			printf("E\n");
-	}
-
-	else if(tarefa == 'X'){ //ERRO,SEMPRE QUE EXECUTA A TAREFA, O x É O MESMO E DEVERIA SER DIFERENTE
-
-		if(verificado){
-
-			while(mdc(r, n) != 1){//não ta entrando após ter sido executado 1x
-
-				srand((unsigned)time(NULL));//falta usar "busca binaria" pra ele ser uniformemente distribuído
-				r = rand();
-				printf("%lu\n", r); //pra testar os r gerados e se entra no laço
-			}
-
-			x = (((r % n) * r) % n); //x Sempre o mesmo, e depois de rodar tarefe P o x passa a ser o mesmo do x da P
-
-			printf("C %lu\n", x);
-			//iniciado = true;
-		}
-		else{
-
-			printf("E\n");
-			exit(0);
-		}
-	}
-
-	else if(tarefa == 'P'){
-
-		if(verificado){
-
-			scanf("%lu", &r);
-
-			x = (((r % n) * r) % n);
-
-			printf("C %lu\n", x);
-		}
-		else{
-
-			printf("E\n");
-			exit(0);
-		}
-	}
-
-	else if(tarefa == 'R'){ //falta corrigir a impressão de erro caso não foi iniciado ainda
-
-		scanf("%d", &b);
-
-		/*if(resp || iniciado == false){
-			printf("E\n");
-			exit(0);
-		}*/
-
-		if(b == 0){
-
-			printf("C %lu\n", r);
-			r = 0;
-			resp = true;
-		}
+		switch(tarefa){
 		
-		else if(b == 1){
+			case 'I':
 
-			y = (((r % n) * s) % n); //tem que adaptar pra caso r 128 bits
-			printf("C %lu\n", y);
-			r = 0;
-			resp = true;
+				scanf("%lu %lu %lu", &n, &s, &v);
+
+				if(((((s * s) % n) * v) % n) == 1){
+					printf("C\n");
+					verificado = true;
+				}
+				else
+					printf("E\n");
+
+			break;
+
+			case 'X': //ERRO,SEMPRE QUE EXECUTA A TAREFA, O x É O MESMO E DEVERIA SER DIFERENTE
+
+				if(verificado){
+
+					while(mdc(r, n) != 1){//não ta entrando após ter sido executado 1x
+
+						srand((unsigned)time(NULL));//falta usar "busca binaria" pra ele ser uniformemente distribuído
+						r = rand();
+						printf("%lu\n", r); //pra testar os r gerados e se entra no laço
+					}
+
+					x = (((r % n) * r) % n); //x Sempre o mesmo, e depois de rodar tarefe P o x passa a ser o mesmo do x da P
+
+					printf("C %lu\n", x);
+					//iniciado = true;
+				}
+				else{
+
+					printf("E\n");
+					exit(0);
+				}
+
+			break;
+
+			case 'P':
+
+				if(verificado){
+
+					scanf("%lu", &r);
+
+					x = (((r % n) * r) % n);
+
+					printf("C %lu\n", x);
+				}
+				else{
+
+					printf("E\n");
+					exit(0);
+				}
+
+			break;
+
+			case 'R': //falta corrigir a impressão de erro caso não foi iniciado ainda e caso ja tiver respondido
+
+				scanf("%d", &b);
+
+				/*if(resp || iniciado == false){
+					printf("E\n");
+					exit(0);
+				}*/
+
+				if(b == 0){
+
+					printf("C %lu\n", r);
+					r = 0;
+					resp = true;
+				}
+
+				else if(b == 1){
+
+					y = (((r % n) * s) % n); //tem que adaptar pra caso r 128 bits
+					printf("C %lu\n", y);
+					r = 0;
+					resp = true;
+				}
+
+				else
+					printf("E\n");
+				
+			break;
 		}
-
-		else
-			printf("E\n");
 	}
-}
+
+	printf("C\n");
+}	
 
 void patricia(char tarefa){
 
@@ -137,13 +152,13 @@ int main(int argc, char *argv[]){
 
 	char tarefa;
 
-	while(tarefa != 'T'){ //o ideal é não sair da função chamada, e não do jeito que está
+	switch(*argv[1]){
 
-		fabio(tarefa);
-		scanf("%c", &tarefa);
+		case 'F':
+
+			fabio(tarefa);
+		break;
 	}
-
-	printf("C\n");
 
 	return 0;
 }
